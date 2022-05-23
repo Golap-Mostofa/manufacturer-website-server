c55604dd230a4311bed7b79c87f6fe2dfe81bc99
Golap-mostofa <mostofa6410@gmail.com>
2022-05-23 18:13:11 +0600

    jwtimplement

--- index.js
+++ index.js
@@ -28,7 +28,16 @@ async function run(){
             res.send(products)
         })
 
-        app.put('/user/:email',async(req,res)={
+        app.put('/user/:email',async(req,res)=>{
+            const email = req.params.email
+            const user = req.body
+            const filter = {email: email}
+            const options = {upsert: true}
+            const updateDoc={
+              $set:user
+            }
+            const result = await userColloction.updateOne(filter,updateDoc,options)
+            res.send(result)
 
         })
 
